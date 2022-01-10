5862825bdf96416f5ba15b1ae74fb5e930271817
patrickdbrowne <patrickdbrowne@gmail.com>
2022-01-10 23:39:57 +1100

    Dictionary, Time, Country info, and weather API I also added other phrases like "challenge bot" which refers to acknowledgements and "meaning of life" reference...

--- ChattyBot/actions/actions.py
+++ ChattyBot/actions/actions.py
@@ -9,6 +9,8 @@ from typing import Any, Text, Dict, List
 import requests
 import json
 
+from requests.api import request
+
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
@@ -22,17 +24,17 @@ class ActionShowTimeZone(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        # gets the latest entity value from the slots "city". E.g., London. returns None if there's nothing
-        place = next(tracker.get_latest_entity_values("city"), None)
-
-        IANA_options = requests.get("https://www.timeapi.io/api/TimeZone/AvailableTimeZones").json()
-
-        #in case user says a continent or country like Australia
-        not_city = ["africa", "america", "antarctica", "asia", "atlantic", "australia", "brazil", "canada", "chile", "etc", "europe", "indian", "mexico", "pacific", "us"]
-        index = -1
-
+        
         try:
+            # gets the latest entity value from the slots "city". E.g., London. returns None if there's nothing
+            place = next(tracker.get_latest_entity_values("city"), None)
+
+            IANA_options = requests.get("https://www.timeapi.io/api/TimeZone/AvailableTimeZones").json()
+
+            #in case user says a continent or country like Australia
+            not_city = ["africa", "america", "antarctica", "asia", "atlantic", "australia", "brazil", "canada", "chile", "etc", "europe", "indian", "mexico", "pacific", "us"]
+            index = -1
+
             #checks the input against each value in the list
             for valid_place in IANA_options:
                 # a value is not in a string if str.find() == -1
@@ -51,15 +53,10 @@ class ActionShowTimeZone(Action):
 
                 response = requests.get("https://www.timeapi.io/api/Time/current/zone", params=parameters)
 
-                # If API is deprecated or unusable
-                if response.status_code != 200:
-                    dispatcher.utter_message(text="Sorry! It seems like there is an issue with the current time zone APIs. In the meantime, why don't you ask something else?")
-                    return []
-            
                 # returns sentence
                 # if user inputs a continent or country, then the first valid answer in the json 
                 # file will be assumed. E.g., instead of America, the bot will respond with America/Adak
-                elif place.lower() in not_city:
+                if place.lower() in not_city:
                 
                     # This returns the message displayed on the screen
                     dispatcher.utter_message(text="The time in {} is {}.".format(IANA_options[index], response.json()['time']))
@@ -80,7 +77,8 @@ class ActionShowTimeZone(Action):
                 dispatcher.utter_message(text="Sorry can you repeat that?")
                 return []
         except:
-            dispatcher.utter_message(text="Sorry can you repeat that? Something went wrong!")
+            # If API is deprecated or unusable
+            dispatcher.utter_message(text="Sorry! It seems like there is an issue with the current time zone APIs. In the meantime, why don't you ask something else?")
             return []
 
 class ActionShowWeather(Action):
@@ -91,25 +89,25 @@ class ActionShowWeather(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        # WEATHER API key
-        weather_key = "f5a120d67a0246e2ad311505220801"
-
-        # default is True if no entity is given
-        default = False
-
-        place = next(tracker.get_latest_entity_values("city"), None)
         
-        if place == None:
-            default = True
-
-        IANA_options = requests.get("https://www.timeapi.io/api/TimeZone/AvailableTimeZones").json()
-
-        #in case user says a continent or country like Australia
-        not_city = ["africa", "america", "antarctica", "asia", "atlantic", "australia", "brazil", "canada", "chile", "etc", "europe", "indian", "mexico", "pacific", "us"]
-        index = -1
-
         try:
+            # WEATHER API key
+            weather_key = "f5a120d67a0246e2ad311505220801"
+
+            # default is True if no entity is given
+            default = False
+
+            place = next(tracker.get_latest_entity_values("city"), None)
+            
+            if place == None:
+                default = True
+
+            IANA_options = requests.get("https://www.timeapi.io/api/TimeZone/AvailableTimeZones").json()
+
+            #in case user says a continent or country like Australia
+            not_city = ["africa", "america", "antarctica", "asia", "atlantic", "australia", "brazil", "canada", "chile", "etc", "europe", "indian", "mexico", "pacific", "us"]
+            index = -1
+        
             if not default:
                 #checks the input against each value in the list
                 for valid_place in IANA_options:
@@ -144,38 +142,67 @@ class ActionShowWeather(Action):
                         dispatcher.utter_message(text="Sorry! It seems like there is an issue with the current weather APIs. In the meantime, why don't you ask something else?")
                         return []
                 
-                    # returns first valid city in country as a sentence from 
+                    # returns first valid city in country as a sentence if the entity is a country or continent
                     elif place.lower() in not_city:
                     
                         # Message displayed on the screen
                         dispatcher.utter_message(text=u"""
-                            The weather in {} is {}:
-                            - wind: {} km/h
-                            - precipitation: {} mm
-                            - humidity: {}
-                            - average temperature: {}\N{DEGREE SIGN}C
-                            """.format(IANA_options[index], text, wind, precipitation, humidity, temperature))
+                        The weather in {} is {}:
+                        - wind: {} km/h
+                        - precipitation: {} mm
+                        - humidity: {}
+                        - average temperature: {}\N{DEGREE SIGN}C
+                        """.format(IANA_options[index], text, wind, precipitation, humidity, temperature))
 
                         return []
 
                     # Returns weather of place given (if valid)
                     else:
                         dispatcher.utter_message(text=u"""
-                            The weather in {} is {}:
-                            - wind: {} km/h
-                            - precipitation: {} mm
-                            - humidity: {}
-                            - average temperature: {}\N{DEGREE SIGN}C
-                            """.format(place.capitalize(), text, wind, precipitation, humidity, temperature))
+                        The weather in {} is {}:
+                        - wind: {} km/h
+                        - precipitation: {} mm
+                        - humidity: {}
+                        - average temperature: {}\N{DEGREE SIGN}C
+                        """.format(place.capitalize(), text, wind, precipitation, humidity, temperature))
                         return []
 
                 # Has an entity but could not retrieve it's corresponding weather
                 elif index == -1:
-                    dispatcher.utter_message(text="Sorry! Looks like I can't find the weather in {}. Want to try again?".format(place))
-                    return []
+                    # Tries to use the entity given - since the "q" key can take a range of values - before saying 
+                    # Information on it doesn't exist
+                    try:
+                        weather_parameters = {
+                        "key": weather_key,
+                        "q": place,
+                        }
+
+                        request_weather = requests.get("http://api.weatherapi.com/v1/forecast.json", params=weather_parameters)
+                        
+                        text = request_weather.json()["current"]["condition"]["text"].lower()
+                        wind = request_weather.json()["current"]["wind_kph"]
+                        precipitation = request_weather.json()["current"]["precip_mm"]
+                        humidity = request_weather.json()["current"]["humidity"]
+                        temperature = request_weather.json()["current"]["temp_c"]
+                        
+                        # Reformat it in case the returned value is a capital city
+                        if place.lower() != request_weather.json()["location"]["name"].lower():
+                            place = str(request_weather.json()["location"]["name"]) + ", " + str(place)
+                        
+                        dispatcher.utter_message(text=u"""
+                        The weather in {} is {}:
+                        - wind: {} km/h
+                        - precipitation: {} mm
+                        - humidity: {}
+                        - average temperature: {}\N{DEGREE SIGN}C
+                        """.format(place, text, wind, precipitation, humidity, temperature))
+                        return []
+
+                    except:
+                        dispatcher.utter_message(text="Sorry! Looks like I can't find the weather in {}. Want to try again?".format(place))
+                        return []
 
             # Does not have entity so it's default - accesses weather by IP address. equivalent to elif default
-
             else:
                 # Weather accessed by IP address if no entity exists
                 weather_parameters = {
@@ -189,15 +216,126 @@ class ActionShowWeather(Action):
                 precipitation = request_weather.json()["current"]["precip_mm"]
                 humidity = request_weather.json()["current"]["humidity"]
                 temperature = request_weather.json()["current"]["temp_c"]
+                location = request_weather.json()["location"]["name"]
 
                 dispatcher.utter_message(text=u"""
-                        According to your IP address, the weather in {} is {}:
-                        - wind: {} km/h
-                        - precipitation: {} mm
-                        - humidity: {}
-                        - average temperature: {}\N{DEGREE SIGN}C
-                        """.format(IANA_options[index], text, wind, precipitation, humidity, temperature))
+                According to your IP address, the weather in {} is {}:
+                - wind: {} km/h
+                - precipitation: {} mm
+                - humidity: {}
+                - average temperature: {}\N{DEGREE SIGN}C
+                """.format(location, text, wind, precipitation, humidity, temperature))
                 return []
         except:
-            dispatcher.utter_message(text="Sorry can you repeat that? Something went wrong!")
+            dispatcher.utter_message(text="Oh no! we think our APIs are deprecated. Want to ask another question?")
+            return []
+
+class ActionShowDefinition(Action):
+    """Returns the definition of a word"""
+    # RECOMMENDED TO SURROUND THE WORD IN QUOTES ("") WHEN TYPING
+    
+    def name(self) -> Text:
+        return "action_show_definition"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        try: 
+            # Algorithm to find the word in "". 
+            users_message = tracker.latest_message.get('text')
+            # gets first "
+            start_word = users_message.index("\"") + 1
+            users_word = users_message[start_word:]
+            # finds second " in start_word
+            end_word = users_word.index("\"")
+
+            # actual word we use for definition
+            word = users_word[:end_word]
+            
+            request_word = requests.get("https://api.dictionaryapi.dev/api/v2/entries/en/{}".format(word))
+            
+            # if request_word.status_code != 200:
+            #     dispatcher.utter_message(text="Oh my! It looks like the current dictionary APIs are deprecated...")
+            #     return []
+            
+            request_word = request_word.json()
+
+            definition = False
+            example = False
+
+            # First check if word exists, since invalid words have the type dictionary, and valid are in lists
+            if isinstance(request_word, dict):
+                dispatcher.utter_message(text="Sorry, Scholar! I can't seem to find a definition for {}.".format(word))
+                return []
+            else:
+                # Checking if the word has a definition
+                if "definition" in request_word[0]["meanings"][0]["definitions"][0].keys():
+                    most_common_definition = request_word[0]["meanings"][0]["definitions"][0]["definition"]
+                    definition = True
+
+                # Checking if the word has an example
+                if "example" in request_word[0]["meanings"][0]["definitions"][0].keys():
+                    example_of_word = request_word[0]["meanings"][0]["definitions"][0]["example"]
+                    example = True
+
+                # Prints this if word has both an example and definition
+                if definition and example:
+                    dispatcher.utter_message(text="""The most common definition of {} is {} For example, {}""".format(word, most_common_definition, example_of_word))
+                    return []
+
+                # Prints this if word only has definition
+                if definition and not example:
+                    dispatcher.utter_message(text="""The most common definition of {} is {} I can't seem to find any examples...""".format(word, most_common_definition))
+                    return []
+        
+        except:
+            dispatcher.utter_message(text="Yikes! It looks like something (our dictionary API) has gone wrong...")
+            return []
+
+class ActionShowCountryInfo(Action):
+    """Returns the information on a country"""
+    
+    def name(self) -> Text:
+        return "action_find_country_info"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    
+        try:
+            place = next(tracker.get_latest_entity_values("city"), None)
+            request_country = requests.get("https://restcountries.com/v3.1/name/{}".format(place)).json()
+
+            if isinstance(request_country, list):
+                official_name = request_country[0]['name']['official']
+                location_link = request_country[0]['maps']['googleMaps']
+
+                # To get currency for any country. The second part is equivalent to "AUD" in australia
+                currency = request_country[0]['currencies'][list(request_country[0]['currencies'].keys())[0]]
+                area = request_country[0]['region']
+                demonym = request_country[0]['demonyms']['eng']['m']
+                population = request_country[0]['population']
+                continent = request_country[0]['continents'][0]
+                capital = request_country[0]['capital'][0]
+
+                # Again, the second component is specific to each country
+                main_language = request_country[0]['languages'][list(request_country[0]['languages'].keys())[0]]
+
+                place = place.capitalize()
+
+
+                dispatcher.utter_message(text="""
+                {}, or officially known as {}, is in the region {} and in the continent {}.
+                {}'s currency is the {} ({} {}). Not only that, but did you know there are {} {}s living there at the moment?! 
+                Additionally, {}'s Capital is {} with the main language being {}.
+                And even though we can't travel there because of COVID, what's stopping us from digitally visiting there with google? {}.
+                """.format(place, official_name, area, continent, place, currency['name'], currency['symbol'], list(request_country[0]['currencies'].keys())[0], population, demonym, place, capital, main_language, location_link))
+                return []
+
+            else:
+                dispatcher.utter_message(text="Oh no! We can't find any info on {}. Have you checked the spelling or made sure you've asked about a country?".format(place))
+                return []
+        except:
+            dispatcher.utter_message(text="Sorry! Something/API has gone wrong...")
             return []
--- testApi.py
+++ testApi.py
@@ -1,6 +1,9 @@
+from typing import List
 import requests
 import json
 import socket
+
+from requests.api import request
 
 # TIME ZONE API
 # place = input("which timezone do u want?\n")
@@ -36,28 +39,163 @@ import socket
 #         print("the time in", place.capitalize(), "is", response.json()['time'])
 
 
-# WEATHER API
-weather_key = "f5a120d67a0246e2ad311505220801"
-# parameters for calling weather json. refer to documentation for "q" since it can hold multiple 
-# data types
-weather_parameters = {
-    "key": weather_key,
-    "q": "auto:ip",
-}
-print(socket.gethostbyname(socket.gethostname()))
+# # WEATHER API
+# weather_key = "f5a120d67a0246e2ad311505220801"
+# # parameters for calling weather json. refer to documentation for "q" since it can hold multiple 
+# # data types
+# weather_parameters = {
+#     "key": weather_key,
+#     "q": "Papua New Guinea",
+# }
+# print(socket.gethostbyname(socket.gethostname()))
 
-# optional parameters include:
-# - "hour" ("hour":5 is 5am)
-# - "days" ("days":5 is number of days of forecast)
-request_weather = requests.get("http://api.weatherapi.com/v1/forecast.json", params=weather_parameters).json()
+# # optional parameters include:
+# # - "hour" ("hour":5 is 5am)
+# # - "days" ("days":5 is number of days of forecast)
+# request_weather = requests.get("http://api.weatherapi.com/v1/forecast.json", params=weather_parameters).json()
 
-# collects the location's wind, precipitation, humidity, and average temperature
-print(u"""
-The weather in {} is {}:
-- wind: {} km/h
-- precipitation: {} mm
-- humidity: {}
-- average temperature: {}\N{DEGREE SIGN}C
-""".format(request_weather["location"]["name"], request_weather["current"]["condition"]["text"].lower(), request_weather["current"]["wind_kph"], request_weather["current"]["precip_mm"], request_weather["current"]["humidity"], request_weather["current"]["temp_c"]))
+# # collects the location's wind, precipitation, humidity, and average temperature
+# print(u"""
+# The weather in {} is {}:
+# - wind: {} km/h
+# - precipitation: {} mm
+# - humidity: {}
+# - average temperature: {}\N{DEGREE SIGN}C
+# """.format(request_weather["location"]["name"], request_weather["current"]["condition"]["text"].lower(), request_weather["current"]["wind_kph"], request_weather["current"]["precip_mm"], request_weather["current"]["humidity"], request_weather["current"]["temp_c"]))
 
-print(request_weather)
+# print(request_weather)
+
+
+# DEFINITIONS API
+
+# word = input("definition of word: ")
+# request_word = requests.get("https://api.dictionaryapi.dev/api/v2/entries/en/{}".format(word))
+
+# print(request_word.status_code)
+# definition = False
+# example = False
+
+# # First check if word exists, since invalid words have the type dictionary, and valid are in lists
+# if isinstance(request_word, dict):
+#     print("Sorry, Scholar! I can't seem to find a definition for {}.".format(word))
+
+# else:
+#     # Checking if the word has a definition
+#     if "definition" in request_word[0]["meanings"][0]["definitions"][0].keys():
+#         most_common_definition = request_word[0]["meanings"][0]["definitions"][0]["definition"]
+#         definition = True
+
+#     # Checking if the word has an example
+#     if "example" in request_word[0]["meanings"][0]["definitions"][0].keys():
+#         example_of_word = request_word[0]["meanings"][0]["definitions"][0]["example"]
+#         example = True
+
+#     # Prints this if word has both an example and definition
+#     if definition and example:
+#         print("""The most common definition of {} is {} For example, {}""".format(word, most_common_definition, example_of_word))
+
+#     # Prints this if word only has definition
+#     if definition and not example:
+#         print("""The most common definition of {} is {} I can't seem to find any examples...""".format(word, most_common_definition))
+
+# users_message = 'define wagly woo "oha m goha" '
+# # gets first "
+# start_word = users_message.index("\"") + 1
+# users_word = users_message[start_word:]
+# # finds second " in start_word
+# end_word = users_word.index("\"")
+
+# print(users_word[:end_word])
+# make sure place is not None
+# place = input("place: ")
+# request_country = requests.get("https://restcountries.com/v3.1/name/{}".format(place)).json()
+
+# if isinstance(request_country, list):
+#     official_name = request_country[0]['name']['official']
+#     location_link = request_country[0]['maps']['googleMaps']
+
+#     # To get currency for any country. The second part is equivalent to "AUD" in australia
+#     print(list(request_country[0]['currencies'].keys()))
+#     currency = request_country[0]['currencies'][list(request_country[0]['currencies'].keys())[0]]
+#     area = request_country[0]['region']
+#     demonym = request_country[0]['demonyms']['eng']['m']
+#     population = request_country[0]['population']
+#     continent = request_country[0]['continents'][0]
+#     capital = request_country[0]['capital'][0]
+
+#     # Again, the second component is specific to each country
+#     main_language = request_country[0]['languages'][list(request_country[0]['languages'].keys())[0]]
+
+#     place = place.capitalize()
+
+
+#     print("""
+#     {}, or officially known as {}, is in the region {} and in the continent {}.
+#     {}'s currency is the {} ({} {}). Not only that, but did you know there are {} {}'s living there at the moment?! 
+#     Additionally, {}'s Capital is {} with the main language being {}.
+#     And even though we can't travel there because of COVID, what's stopping us from digitally visiting there with google? {}.
+#     """.format(place, official_name, area, continent, place, currency['name'], currency['symbol'], list(request_country[0]['currencies'].keys())[0], population, demonym, place, capital, main_language, location_link))
+
+#     print()
+# else:
+#     print("howdy buddy. We can't find any info on {}".format(place))
+
+# link = "https://api.covid19api.com/summary"
+
+# request_COVID = requests.get(link).json()
+
+# if place == None
+# global_new_cases = request_COVID["Global"]["NewConfirmed"]
+# global_total_cases = request_COVID["Global"]["TotalConfirmed"]
+# global_total_deaths = request_COVID["Global"]["TotalDeaths"]
+# global_total_recoveries = request_COVID["Global"]["TotalRecovered"]
+# month_dict = {
+#     '01':'January',
+#     '02':'February',
+#     '03':'March',
+#     '04':'April',
+#     '05':'May',
+#     '06':'June',
+#     '07':'July',
+#     '08':'August',
+#     '09':'September',
+#     '10':'October',
+#     '11':'November',
+#     '12':'December'
+#     }
+
+# date = request_COVID["Date"]
+# year = date[0:4]
+# month = month_dict[date[5:7]]
+# day = date[8:10]
+
+
+# print("""
+# Today, there were {} confirmed cases globally! In total, there are {} cases, {} deaths, and {} recoveries in the world.
+# This data was last updated on {} {}, {}.""".format(global_new_cases, global_total_cases, global_total_deaths, global_total_recoveries, day, month, year))
+
+# if place != None
+# place = "united states of america"
+# Keys are not country names, so I need to identify index of country's info
+# list_countries = request_COVID["Countries"]
+# country_index = None
+
+# for index in list_countries:
+#     if place.lower() in index["Country"].lower():
+#         country_index = list_countries.index(index)
+#         break
+#     else:
+#         continue
+
+# if country_index != None:
+
+#     print(request_COVID["Countries"][country_index])
+#     place_new_cases = request_COVID["Countries"][country_index]["NewConfirmed"]
+#     place_total_cases = request_COVID["Countries"][country_index]["TotalConfirmed"]
+#     place_total_deaths = request_COVID["Countries"][country_index]["TotalDeaths"]
+#     place_total_recoveries = request_COVID["Countries"][country_index]["TotalRecovered"]
+#     print("""
+#     In {}, there were {} cases today! So far, there are {} cases, {} deaths, and {} recoveries in {}.
+#     This data was last updated on {} {}, {}""".format(place, place_new_cases, place_total_cases, place_total_deaths, place_total_recoveries, place, day, month, year))
+# elif country_index == None:
+#     print("Uh oh! Looks like I can't find any COVID data for {}. Have you checked the spelling?".format(place))
